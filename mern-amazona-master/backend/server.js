b60ce7874e032a9dde3bf90c15faef44df1881af
FerIbarra1 <97769653+FerIbarra1@users.noreply.github.com>
2022-06-06 19:26:28 -0700

    master

--- mern-amazona-master/backend/server.js
+++ mern-amazona-master/backend/server.js
@@ -18,11 +18,8 @@ mongoose
     console.log(err.message);
   });
 
-const cors = require('cors')
 
 const app = express();
-
-app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
