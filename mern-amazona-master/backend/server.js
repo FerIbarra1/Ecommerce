c0adb385320bdf06ef1cdb8555193639888986aa
FerIbarra1 <97769653+FerIbarra1@users.noreply.github.com>
2022-06-06 22:47:43 -0700

    master

--- mern-amazona-master/backend/server.js
+++ mern-amazona-master/backend/server.js
@@ -21,10 +21,21 @@ mongoose
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
-  credentials: true,
-}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
